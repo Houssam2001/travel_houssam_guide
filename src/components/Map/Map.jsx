e15707bd56d46096e19ab9fb9b60fa7576e454a0
Houssam2001 <37514580+Houssam2001@users.noreply.github.com>
2022-01-18 13:32:23 +0000

    FirstCommit

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,10 +5,24 @@ import {LocationOnOutlined} from "@material-ui/icons";
 import {Rating} from "@material-ui/lab";
 import useStyle from './styles'
 const Map = () => {
+    const classes = useStyle()
+    const  isMobile = useMediaQuery('(min-width:600px)')
+    const  coordinates={lat:0,lng:0}
     return (
-        <h1>
-            Map
-        </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{key:'AIzaSyCG0300rijWIBH2J1Wx0c_6M1pOzj0lxVM'}}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 export default Map
